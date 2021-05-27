9a1db9d0fd20ce480f8818eb9bba751b174288a7
gohittanya <gohittashu95@gmail.com>
2021-05-27 15:03:09 +0000

    Commit

---
+++ src/main/java/com/example/test/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.test.repository;
+
+import java.util.List;
+
+import com.example.test.model.Tutorial;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+  List<Tutorial> findByTitleContaining(String title);
+  List<Tutorial> findByPublished(boolean published);
+}
